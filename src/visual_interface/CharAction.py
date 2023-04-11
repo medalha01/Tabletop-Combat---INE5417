d0001e3a5b31f392ae9dec37102f2a36ba8e8038
Tiago Luz <tiagoluz64@gmail.com>
2023-04-11 14:01:49 -0300

    improved GUI. added sidebar class. new GUI organization

--- src/visual_interface/CharAction.py
+++ src/visual_interface/CharAction.py
@@ -8,5 +8,5 @@ import random
 class CharAction(Widget):
     def __init__(self, window) -> None:
         super().__init__(window)
-        self._text = tk.Label(self.frame, text=f"thingamajing {random.random()}")
+        self._text = tk.Label(self.frame, text=f"Action {random.randint(0,100)}")
         self._text.pack(side="top", anchor='w')
