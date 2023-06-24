752097b8b2aa5cdc834102dcba626ec790ef1cba
Isac Martins <hiperboss2009@gmail.com>
2023-06-24 14:19:55 -0300

    Dropdown Implementado

--- VirtualTableTop/game_logic/visual_interface/windows.py
+++ VirtualTableTop/game_logic/visual_interface/windows.py
@@ -95,6 +95,36 @@ class CreateCharWindow(AuxWindow):
 
     def get_status_labels(self):
         return ["Name", "Color", "Position", "Level", "HP", "Initiative", "CA", "Speed"]
+    
+    def create_textboxes(self):
+        status = self.get_status_labels()
+        self.dropDownList = []
+        for i, label in enumerate(status):
+
+            label_frame = tk.Frame(self.window, bg="#3B3B3B", padx=5, pady=5)
+            label_frame.pack(side="top", fill="x", padx=10, expand=True)  # ???
+            textbox_label = tk.Label(
+            label_frame,
+            text=label,
+            font=("helvetica", 12),
+            bg="#3B3B3B",
+            fg="#e3e3e3",
+            width=10,
+            anchor="w"  # ancora no canto esquerdo
+                )
+            textbox_label.pack(side="left", padx=(0, 10))
+            if label == "Color":
+                options = ["Red", "Blue", "Purple", "Yellow", "Green", "Orange", "Pink", "Brown", "Black", "White", "Gray"]
+                dropdown = tk.StringVar(label_frame)
+                dropdown.set('Red')
+                dropdown_menu = tk.OptionMenu(label_frame, dropdown, *options)
+                dropdown_menu.pack(side="top", fill="x", padx=10, expand=True)
+                self.dropDownList.append(dropdown)
+            else:
+                textbox = tk.Entry(label_frame)
+                textbox.pack(side="right", expand=True, fill="x", padx=5)
+                self.textbox_entries[label] = textbox
+                self.window.configure(bg=textbox_label["bg"])
 
     def open_window(self):
         self.textbox_entries["actions"] = []
@@ -114,7 +144,7 @@ class CreateCharWindow(AuxWindow):
     
     def retrieve_values(self):
         try:
-            char = {"name": self.textbox_entries["Name"].get(), "color": self.textbox_entries["Color"].get(), "position": tuple(map(int, self.textbox_entries["Position"].get().split(','))), 
+            char = {"name": self.textbox_entries["Name"].get(), "color": self.dropDownList[0].get(), "position": tuple(map(int, self.textbox_entries["Position"].get().split(','))), 
                     "level": self.textbox_entries["Level"].get(), "hp_max": int(self.textbox_entries["HP"].get()), 
                     "initiative": int(self.textbox_entries["Initiative"].get()), "ca": int(self.textbox_entries["CA"].get()), 
                     "speed": float(self.textbox_entries["Speed"].get()), "actions": self.textbox_entries["actions"]}
